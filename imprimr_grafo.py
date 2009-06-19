99a114e9bc92748dd6fe9a53cf1efc4890e3f00d
rodrigocc <rodrigocc@368b3fa0-5380-11de-83c8-e5b33934001f>
2009-06-19 02:47:48 +0000

    Agregar una funcion para imprimir un grafo bonitamente
    Depende de dot (de graphviz: www.graphviz.org). Para usarlo simple como:
    
    python test_sd.py  > g.dot (si en test_sd.py llamamos a imprimir(g))
    dot -Tpng g.dot > g.png
    
    ver el png
    
    OJO: en el archivo de python no pueden haber prints de debug, porque formarian
    parte del archivo de dot y la cagan

---
+++ imprimr_grafo.py
@@ -0,0 +1,48 @@
+from graph import *
+from sd import *
+
+def imprimir(g):
+
+	print "digraph finite_state_machine {"
+	print "rankdir=LR;"
+	print 'size="8,5"'
+
+	# marcamos con doble circulo al distinguido
+	print "node [shape = doublecircle];", char2str(g.root.char) + "_" + str(g.root.id)
+	print "node [shape = circle];"
+
+
+	def f(node):
+
+		for h in node.links:
+
+			# No usamos solo el char, porque esto "identifica" al
+			# nodo. Y no usamos solo el id porque eso no nos dice
+			# nada a la vista. Entonces, combinamos los dos =)
+			# Y usamos char2str porque los '.' y esos no son validos
+			# en dot para nombre de nodos
+			print char2str(node.char) + "_" + str(node.id), "->", \
+					char2str(h.char) + "_" + str(h.id)
+	
+	# FIN
+	iterar_grafo(g, f)
+
+	print "}"
+
+
+def char2str(c):
+
+	if c == '.':
+		return"punto"
+	elif c == '+':
+		return "mas"
+	elif c == '?':
+		return "preg"
+	elif c == '*':
+		return "asterisco"
+	elif c == '\\':
+		return "lambda"
+	else:
+		return c
+
+
