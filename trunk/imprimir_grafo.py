25eeb8a27b3bae3f677a0b6a82688b83307bf2e9
rodrigocc <rodrigocc@368b3fa0-5380-11de-83c8-e5b33934001f>
2009-06-20 17:42:28 +0000

    imprimir_grafo: dejar que dot adivine el tamaño, que lo hace bien
    Con grafos grandes y fijando un tamaño, la cagabamos (no se veian bien los
    nodos)

--- trunk/imprimir_grafo.py
+++ trunk/imprimir_grafo.py
@@ -4,7 +4,7 @@ from sd import *
 def imprimir(g):
 
 	print "digraph finite_state_machine {"
-	print 'size="8,5"'
+	#print 'size="8,5"'
 
 	# marcamos con doble circulo al distinguido
 	print "node [shape = doublecircle];", g.root.id
